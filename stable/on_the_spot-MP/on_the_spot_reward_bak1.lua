9db97100418c5434e0ef5d71d5be32648fbb624e
FabriceTUe <f.e.kusters@student.tue.nl>
2024-08-06 19:51:26 +0200

    updated on_the_spot data/scenario

--- stable/on_the_spot-MP/on_the_spot_reward_bak1.lua
+++ stable/on_the_spot-MP/on_the_spot_reward_bak1.lua
@@ -4,7 +4,7 @@ function reward()
     local reward = 0
     
     -- check if a mistake was made:
-    if data.error_4 > 1 then
+    if data.error_3 == 90402614005800 then
         reward = reward - 1 -- punish model for a misstep
     end
 
@@ -12,7 +12,7 @@ function reward()
     local current_stage = math.fmod(data.stage, 10)
     if current_stage > previous_stage then
         previous_stage = current_stage
-        reward = reward + 1 -- reward model for progressing
+        reward = reward + 5
     end
 
     return reward
