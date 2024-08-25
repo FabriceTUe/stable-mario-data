dae05616c1931355b18a8e8f42d50db4e7afc17a
FabriceTUe <f.e.kusters@student.tue.nl>
2024-08-25 17:53:05 +0200

    adapted on_the_spot reward

--- stable/on_the_spot-MP/on_the_spot_reward.lua
+++ stable/on_the_spot-MP/on_the_spot_reward.lua
@@ -1,18 +1,23 @@
 previous_stage = 1
+stage_mod = 10
+previous_count = 0
+count_mod = 100000000
 
 function reward()
     local reward = 0
     
-    -- check if a mistake was made:
-    if data.error_4 > 1 then
-        --reward = reward - 1 -- punish model for a misstep
+    -- check if progress was made:
+    local current_count = data.count / count_mod
+    if current_count > previous_count then
+        reward = reward + 1 -- reward progress
+        previous_count = current_count
     end
 
     -- check if model progressed to a next state:
-    local current_stage = math.fmod(data.stage, 10)
+    local current_stage = math.fmod(data.stage, stage_mod)
     if current_stage > previous_stage then
+        previous_count = 0 -- reset progress
         previous_stage = current_stage
-        reward = reward + 1 -- reward model for progressing
     end
 
     return reward
