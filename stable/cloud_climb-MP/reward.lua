a7d184cac379637e1711b60c3b93079b3cad8f3f
VincentDekkers <v.d.n.dekkers@gmail.com>
2024-10-07 21:47:04 +0200

    Added forest jump, drop em and finished cloud climb

--- stable/cloud_climb-MP/reward.lua
+++ stable/cloud_climb-MP/reward.lua
@@ -1,16 +1,16 @@
+tottime = 0
 function reward()
-    local height = data.height
     local reward
-    local maxheight
-    if maxheight ~= nil then
-        height = maxheight
+    local height = data.height
+    reward = -0.000000000001*(height - 2700 - ((1897-2880)/2800)*tottime)^3
+    if height < 2200 and reward > 0 then
+        reward = reward*2
+    elseif height < 2200 and reward < 0 then
+        reward = reward*0.5
     end
-    if height < maxheight then
-        maxheight = height
-        reward = 0.01
-    elseif height - 100 < maxheight then
-        reward = -0.0001
-    else
-        reward = -0.0001 +(maxheight - height + 100)/10000
+    if height < 1900 and data.xcoordinate < 140 and data.xcoordinate > 100 then
+        reward = reward + (5400-tottime)/800
     end
+    tottime = tottime + 1
+    return reward
 end
