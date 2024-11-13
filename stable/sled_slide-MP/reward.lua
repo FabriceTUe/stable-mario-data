4d9a1169c828d7eb26e3675b88c1c5c3d3440c85
Vincent Dekkers <v.d.n.dekkers@gmail.com>
2024-11-13 12:56:30 +0100

    fixed sled slide

--- stable/sled_slide-MP/reward.lua
+++ stable/sled_slide-MP/reward.lua
@@ -1,13 +1,14 @@
 lastsegment = 0
+
 function reward()
     local reward
     local progress = data.progress
     if progress - lastsegment == 14 then
         reward = 0
     else
-        reward = 0.1*(progress - lastsegment)
+        reward = 0.3*(progress - lastsegment)
         lastsegment = progress
     end
-    reward = reward - 0.0001
+    reward = reward - 0.0001 + 0.000001*(data.speed)
     return reward 
 end
